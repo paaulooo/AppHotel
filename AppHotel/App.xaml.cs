744ba01d29869b609869e6e0fda8779f21634510
paulo <paulorobertofer98@gmail.com>
2025-10-31 13:21:45 -0300

    Adicionar arquivos de projeto.

---
+++ AppHotel/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace AppHotel
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ AppHotel/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace AppHotel
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
